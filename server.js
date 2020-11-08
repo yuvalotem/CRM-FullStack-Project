01c71a3036131fe3db178d7711751fb50dd65b17
yuvalotem <yuvalotem@gmail.com>
2020-11-08 18:27:13 +0200

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-// app.use('/', api)
+app.use('/', api)
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
