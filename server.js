38a3ee3fe07caee8be5193afe8cc3f66d72dda6d
yuvalotem <yuvalotem@gmail.com>
2020-11-08 17:50:46 +0200

    make it better

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.use('/', api)
+// app.use('/', api)
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
 const port = 4000
 app.listen(process.env.PORT || port, function(req, res){
     console.log('running on ' + port);
